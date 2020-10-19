6c700949da3b55ff70d802918ad25cbeab108694
Megan <megan.ismaili@fshnstudent.info>
2020-10-19 09:32:07 +0200

    comit1

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('home','HomeController@index');
 
-Route::get('about', function () {
-    return view('about');
-});
+// Route::get('home', [ 'as' => 'pagae.welcome', 'uses' => 'HomeController@index' ]);
+
+// Route::get('about', [ 'as' => 'page.about', 'uses' => 'PageController@about' ]);
